02c6f3bd08a3bb3dd993b0bd76b6d637c8b32563
doltechgit <doltechitsolutions@gmail.com>
2023-08-18 12:11:04 +0100

    Fixed Transaction Import

--- app/Exports/TransactionUpdateExport.php
+++ app/Exports/TransactionUpdateExport.php
@@ -4,8 +4,9 @@ namespace App\Exports;
 
 use App\Models\Transaction;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TransactionUpdateExport implements FromCollection
+class TransactionUpdateExport implements FromCollection,WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -13,5 +14,25 @@ class TransactionUpdateExport implements FromCollection
     public function collection()
     {
         return Transaction::all();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'id',
+            'transaction_id',
+            'product_id',
+            'store_id',
+            'client_id',
+            'user_id',
+            'quantity',
+            'price',
+            'discount',
+            'paid',
+            'balance',
+            'pay_method',
+            'created_at',
+            'updated_at'
+        ];
     }
 }
